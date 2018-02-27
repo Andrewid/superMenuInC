a1f614380ca196fec5fbb54aebbc0f4a3b7af2ad
Andrew Merrell <andrewmerrell1@weber.edu>
2018-02-27 11:55:34 -0700

    Inital commit

---
+++ superMenu.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  superMenu.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/27/2018 11:50:50 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Andrew Merrell (), andrewmerrell1@weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    printf("Just work already\n");
+    return 0;
+}
+// Function Definitions
+
+
